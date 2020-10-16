9de8bf6c8f3f553b152781f12631a344386ac4f0
Mauricio Flores Hernandez <mau1361317@gmail.com>
2020-10-15 20:09:56 -0500

    unit test

---
+++ tests/Feature/CategoryTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Tests\Feature;
+
+use App\Models\Category;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Tests\TestCase;
+
+class CategoryTest  extends TestCase
+{
+    use DatabaseMigrations;
+
+    protected $endpoint = 'api/category';
+
+    public function testCreateCategory(){
+        $category = factory(Category::class)->create()->toArray();
+        $response = $this->json('POST', $this->endpoint, $category);
+        $response->assertCreated();
+    }
+    public function testGetAll(){
+        $category = factory(Category::class)->create();
+        $response = $this->json('GET',$this->endpoint);
+        $response->assertJsonFragment(['data'=>[$category->toArray()]]);
+    }
+    public function testUpdateCategory(){
+        $category = factory(Category::class)->create()->toArray();
+        $category['name'] = 'updated';
+        $response = $this->json('PUT', $this->endpoint . '/' .$category['id'], $category);
+        $response->assertJsonFragment(['data' => $category]);
+    }
+    public function testDeleteCategory(){
+        $category = factory(Category::class)->create();
+        $response = $this->json('DELETE', $this->endpoint . '/' .$category->id);
+        $response->assertJsonFragment(['data' => $category->toArray()]);
+    }
+
+
+}
